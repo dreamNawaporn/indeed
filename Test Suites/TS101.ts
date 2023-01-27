13daad2b421ea43968446cd612258cce5c516168
dreamNawaporn <96407096+dreamNawaporn@users.noreply.github.com>
2023-01-27 12:03:24 +0700

    Initial commit

---
+++ Test Suites/TS101.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS101</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fe0a0332-d0c5-48ad-a087-8629767059f9</testSuiteGuid>
+   <testCaseLink>
+      <guid>f83d629a-6399-4e34-b68d-759143e51141</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1003 Search Exception</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
